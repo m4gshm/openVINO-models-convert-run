21474a885183a909ea87a5de354763b11645bd37
Alexander Bulgakov <mfourgeneralsherman@gmail.com>
2026-05-12 23:20:07 +0300

    qwen

---
+++ src_models/init-Qwen2.5-Coder-0.5B-Instruct-int8-g-1.bat
@@ -0,0 +1,20 @@
+set MODEL_NAME=Qwen2.5-Coder-0.5B-Instruct
+set MODEL_NAME_OUT=%MODEL_NAME%
+set MODEL_DEVELOPER=Qwen
+set MODEL_PATH=./%MODEL_DEVELOPER%/%MODEL_NAME%
+set OUTPUT_DIR=../models/%MODEL_NAME_OUT%
+
+set GROUP_SIZE=-1
+set WEIGHT_FORMAT=int8
+
+optimum-cli export openvino ^
+  --model %MODEL_PATH% ^
+  --task text-generation-with-past ^
+  --weight-format %WEIGHT_FORMAT% ^
+  --sym ^
+  --group-size %GROUP_SIZE% ^
+  --ratio 1.0 ^
+  --trust-remote-code ^
+  %OUTPUT_DIR%-%WEIGHT_FORMAT%-sym-g%GROUP_SIZE%-r1/1
+
+pause
