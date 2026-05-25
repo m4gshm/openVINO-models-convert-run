410024f7f12802ac7c02fab1b1d6f37f01f22e63
m4gshm <mfourgeneralsherman@gmail.com>
2026-05-25 15:42:37 +0300

    Qwen3-coder

---
+++ src_models/init-Qwen3-Coder-30B-A3B-Instruct-int4-sym-g128-se-awq.bat
@@ -0,0 +1,22 @@
+set MODEL_NAME=Qwen3-Coder-30B-A3B-Instruct
+set MODEL_DEVELOPER=Qwen
+set MODEL_NAME_OUT=%MODEL_NAME%
+set MODEL_PATH=./%MODEL_DEVELOPER%/%MODEL_NAME%
+set OUTPUT_DIR=../models/%MODEL_NAME_OUT%
+
+set GROUP_SIZE=128
+set WEIGHT_FORMAT=int4
+
+optimum-cli export openvino ^
+  --model %MODEL_PATH% ^
+  --task text-generation-with-past ^
+  --weight-format %WEIGHT_FORMAT% ^
+  --sym ^
+  --group-size %GROUP_SIZE% ^
+  --trust-remote-code ^
+  --dataset gsm8k ^
+  --scale-estimation ^
+  --awq ^
+  %OUTPUT_DIR%-%WEIGHT_FORMAT%-sym-g%GROUP_SIZE%-se-awq/1
+
+pause
