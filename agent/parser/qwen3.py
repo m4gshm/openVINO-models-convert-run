60c0be898eb385486a019e61fa70aabbc9681f22
Alexander Bulgakov <mfourgeneralsherman@gmail.com>
2026-06-13 22:21:36 +0300

    tool call tests

--- agent/parser/qwen3.py
+++ agent/parser/qwen3.py
@@ -7,7 +7,10 @@ from typing import List, Any
 
 import json_repair
 
-from agent.common.openai_model import ToolCall, FunctionCall, FunctionDefinition
+from common.openai_model import ToolCall, FunctionCall, FunctionDefinition
+
+EXPECTED_PROPERTY_TYPE = 'type'
+EXPECTED_PARAMETERS_PROPERTIES = "properties"
 
 TOOL_CALL_START = "<tool_call>"
 TOOL_CALL_END = "</tool_call>"
@@ -50,7 +53,7 @@ def is_prompt_start_thinking(prompt: str) -> bool:
         '\n') else prompt.endswith(REASONING_START)
 
 
-def parse_tool_calls(text: str, supported_functions: dict[str, FunctionDefinition]) -> List[ToolCall]:
+def parse_tool_calls(text: str, supported_functions: dict[str, FunctionDefinition] | None = None) -> List[ToolCall]:
     """Parses Qwen3-Coder style unique XML tool call blocks."""
     tool_call_blocks = text.split(TOOL_CALL_START)
     ts = int(time.time())
@@ -62,9 +65,7 @@ def parse_tool_calls(text: str, supported_functions: dict[str, FunctionDefinitio
             # log
             continue
 
-        function = supported_functions.get(func_name)
-        if function is None:
-            log.warning(f"parsed unsupported function '{func_name}'")
+        function = supported_functions.get(func_name) if supported_functions else None
 
         parameters = function.parameters if function is not None else []
         arguments = get_arguments(call_block, parameters)
@@ -76,7 +77,8 @@ def parse_tool_calls(text: str, supported_functions: dict[str, FunctionDefinitio
 
 
 def get_arguments(call_block: str, expected_parameters: dict[str, Any] | None = None) -> str:
-    expected_properties: dict[str, Any] = expected_parameters.get("properties", {}) if expected_parameters else {}
+    expected_properties: dict[str, Any] = expected_parameters.get(EXPECTED_PARAMETERS_PROPERTIES,
+                                                                  {}) if expected_parameters else {}
 
     param_pattern = r"<parameter=(.*?)>(.*?)</parameter>"
     parameters = re.findall(param_pattern, call_block, re.DOTALL)
@@ -86,15 +88,15 @@ def get_arguments(call_block: str, expected_parameters: dict[str, Any] | None = 
         param_name_norm: str = param_name.strip()
         param_value_norm: str = param_value.strip()
         expected_property: dict[str, Any] = expected_properties.get(param_name_norm, {})
-        expected_type = expected_property.get('type', 'string') if expected_property else 'string'
+        expected_type = expected_property.get(EXPECTED_PROPERTY_TYPE, 'string') if expected_property else 'string'
         if expected_type == 'array' or expected_type == 'object':
             try:
                 structured_parameter: dict[str, Any] | list[Any] = json.loads(param_value_norm)
             except json.decoder.JSONDecodeError as e:
-                log.error(f"function parameter parsing error, parameter '{param_name}', value '{param_value_norm}', "
+                log.debug(f"function parameter parsing error, parameter '{param_name}', value '{param_value_norm}', "
                           f"expected_type '{expected_type}': {e}")
                 structured_parameter: dict[str, Any] | list[Any] = json_repair.loads(param_value_norm)
-                log.info(f"repaired parameter value '{structured_parameter}'")
+                log.debug(f"repaired parameter value '{structured_parameter}'")
 
             arguments[param_name_norm] = structured_parameter
         else:
--- agent/parser/qwen3_test.py
+++ agent/parser/qwen3_test.py
@@ -1,7 +1,7 @@
 import unittest
 
-from common.openai_model import ToolCall
-from parser.qwen3 import parse_tool_calls
+from common.openai_model import ToolCall, FunctionDefinition
+from parser.qwen3 import parse_tool_calls, EXPECTED_PARAMETERS_PROPERTIES, EXPECTED_PROPERTY_TYPE
 
 
 class TestAddFunction(unittest.TestCase):
@@ -32,47 +32,67 @@ class TestAddFunction(unittest.TestCase):
             </function>
             </tool_call>
             """
+        self.function_with_invalid_json_parameter = """
+            <tool_call>
+            <function=select>
+            <parameter=options>
+            [1,2,"3"],
+            </parameter>
+            </function>
+            </tool_call>
+            """
 
     def test_without_close_tag(self):
         calls = parse_tool_calls(self.first_function_no_close_tag)
         self.assertEqual(len(calls), 1)
         function_call: ToolCall = calls[0]
-        self.assertEqual(function_call.type, "function")
-        self.assertEqual(function_call.function.name, "read_file")
-        self.assertEqual(function_call.function.arguments,
-                         """{"end_line": "75", "start_line": "19", "target_file": "Target.py"}""")
+        self.assertEqual("function", function_call.type)
+        self.assertEqual("read_file", function_call.function.name)
+        self.assertEqual("""{"end_line": "75", "start_line": "19", "target_file": "Target.py"}""",
+                         function_call.function.arguments)
 
     def test_two_functions(self):
         calls = parse_tool_calls(self.first_function + self.second_function)
         self.assertEqual(len(calls), 2)
 
         first: ToolCall = calls[0]
-        self.assertEqual(first.type, "function")
-        self.assertEqual(first.function.name, "read_file")
-        self.assertEqual(first.function.arguments,
-                         """{"end_line": "75", "start_line": "19", "target_file": "Target.py"}""")
+        self.assertEqual("function", first.type)
+        self.assertEqual("read_file", first.function.name)
+        self.assertEqual("""{"end_line": "75", "start_line": "19", "target_file": "Target.py"}""",
+                         first.function.arguments)
 
         second: ToolCall = calls[1]
-        self.assertEqual(second.type, "function")
-        self.assertEqual(second.function.name, "ls")
-        self.assertEqual(second.function.arguments,
-                         """{"directory": "/tmp"}""")
+        self.assertEqual("function", second.type)
+        self.assertEqual("ls", second.function.name)
+        self.assertEqual("""{"directory": "/tmp"}""", second.function.arguments)
 
     def test_two_functions_where_first_without_close_tag(self):
         calls = parse_tool_calls(self.first_function_no_close_tag + self.second_function)
         self.assertEqual(len(calls), 2)
 
         first: ToolCall = calls[0]
-        self.assertEqual(first.type, "function")
-        self.assertEqual(first.function.name, "read_file")
-        self.assertEqual(first.function.arguments,
-                         """{"end_line": "75", "start_line": "19", "target_file": "Target.py"}""")
+        self.assertEqual("function", first.type)
+        self.assertEqual("read_file", first.function.name)
+        self.assertEqual("""{"end_line": "75", "start_line": "19", "target_file": "Target.py"}""",
+                         first.function.arguments)
 
         second: ToolCall = calls[1]
-        self.assertEqual(second.type, "function")
-        self.assertEqual(second.function.name, "ls")
-        self.assertEqual(second.function.arguments,
-                         """{"directory": "/tmp"}""")
+        self.assertEqual("function", second.type)
+        self.assertEqual("ls", second.function.name)
+        self.assertEqual("""{"directory": "/tmp"}""", second.function.arguments)
+
+    def test_functions_with_invalid_json_parameter(self):
+        function_name = "select"
+        calls = parse_tool_calls(self.function_with_invalid_json_parameter, {
+            function_name: FunctionDefinition(name=function_name, parameters={
+                EXPECTED_PARAMETERS_PROPERTIES: {"options": {EXPECTED_PROPERTY_TYPE: "array"}}
+            })
+        })
+
+        first: ToolCall = calls[0]
+        self.assertEqual("function", first.type)
+        self.assertEqual("select", first.function.name)
+        self.assertEqual("""{"options": [1, 2, \"3\"]}""", first.function.arguments)
 
 
 if __name__ == '__main__':
