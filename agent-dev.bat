4a04dc6d85b5832e6f68ffc2b596655eb443630e
Alexander Bulgakov <mfourgeneralsherman@gmail.com>
2026-08-04 10:58:46 +0300

    scripts

--- agent-dev.bat
+++ agent-dev.bat
@@ -1,5 +1,5 @@
 IF not defined OPENVINO_DEV_NAME (
-    set OPENVINO_DEV_NAME=openvino_genai_windows_2026.4.0.0.dev20260730_x86_64
+    set OPENVINO_DEV_NAME=openvino_genai_windows_2026.4.0.0.dev20260803_x86_64
 )
 
 if not defined INTEL_OPENVINO_DIR (
