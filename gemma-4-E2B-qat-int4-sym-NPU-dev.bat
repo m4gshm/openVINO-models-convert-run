df3119efdfc7823b189777f2682da4efa8a39e39
Alexander Bulgakov <mfourgeneralsherman@gmail.com>
2026-08-12 23:54:41 +0300

    fix(gen): gemma4

---
+++ gemma-4-E2B-qat-int4-sym-NPU-dev.bat
@@ -0,0 +1,6 @@
+call agent-dev.bat --model gemma-4-E2B-it-qat-q4_0-unquantized-int4-sym-g128 ^
+ --device NPU ^
+ --max_prompt_len 49152 ^
+ --attention_backend PA ^
+ --generate_config_file .config/generate_config_gemma4_npu.json ^
+ --chat_template_file .config/gemma4_chat_template.jinja
