c9f9043592bea304a1d99ab779abeb56a64e4686
Alexander Bulgakov <mfourgeneralsherman@gmail.com>
2026-08-12 23:51:33 +0300

    fix(gen): gemma4

---
+++ src_models/init-gemma-4-E2B-it-qat-int4-sym-g128.bat
@@ -0,0 +1,52 @@
+set MODEL_NAME=gemma-4-E2B-it-qat-q4_0-unquantized
+set MODEL_DEVELOPER=google
+set MODEL_NAME_OUT=%MODEL_NAME%
+set MODEL_PATH=./%MODEL_DEVELOPER%/%MODEL_NAME%
+set OUTPUT_DIR=../models/%MODEL_NAME_OUT%
+
+set GROUP_SIZE=128
+set WEIGHT_FORMAT=int4
+
+optimum-cli export openvino ^
+  --model %MODEL_PATH% ^
+  --task image-text-to-text ^
+  --weight-format %WEIGHT_FORMAT% ^
+  --backup-precision int8_sym ^
+  --group-size %GROUP_SIZE% ^
+  --trust-remote-code ^
+  --dataset textvqa ^
+  --sym ^
+  %OUTPUT_DIR%-%WEIGHT_FORMAT%-sym-g%GROUP_SIZE%
+
+pause
+
+@REM INFO:nncf:Statistics of the bitwidth distribution:
+@REM +---------------------------+-----------------------------+----------------------------------------+
+@REM | Weight compression mode   | % all parameters (layers)   | % ratio-defining parameters (layers)   |
+@REM +===========================+=============================+========================================+
+@REM | int8_sym, per-channel     | 18% (1 / 277)               | 0% (0 / 276)                           |
+@REM +---------------------------+-----------------------------+----------------------------------------+
+@REM | int4_sym, group size 128  | 82% (276 / 277)             | 100% (276 / 276)                       |
+@REM +---------------------------+-----------------------------+----------------------------------------+
+@REM Applying Weight Compression ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 100% • 0:01:16 • 0:00:00
+@REM INFO:nncf:Statistics of the bitwidth distribution:
+@REM +---------------------------+-----------------------------+----------------------------------------+
+@REM | Weight compression mode   | % all parameters (layers)   | % ratio-defining parameters (layers)   |
+@REM +===========================+=============================+========================================+
+@REM | int8_sym, per-channel     | 100% (1 / 1)                | 100% (1 / 1)                           |
+@REM +---------------------------+-----------------------------+----------------------------------------+
+@REM Applying Weight Compression ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 100% • 0:00:05 • 0:00:00
+@REM INFO:nncf:Statistics of the bitwidth distribution:
+@REM +---------------------------+-----------------------------+----------------------------------------+
+@REM | Weight compression mode   | % all parameters (layers)   | % ratio-defining parameters (layers)   |
+@REM +===========================+=============================+========================================+
+@REM | int8_sym, per-channel     | 100% (115 / 115)            | 100% (115 / 115)                       |
+@REM +---------------------------+-----------------------------+----------------------------------------+
+@REM Applying Weight Compression ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 100% • 0:00:09 • 0:00:00
+@REM INFO:nncf:Statistics of the bitwidth distribution:
+@REM +---------------------------+-----------------------------+----------------------------------------+
+@REM | Weight compression mode   | % all parameters (layers)   | % ratio-defining parameters (layers)   |
+@REM +===========================+=============================+========================================+
+@REM | int8_sym, per-channel     | 100% (1 / 1)                | 100% (1 / 1)                           |
+@REM +---------------------------+-----------------------------+----------------------------------------+
+@REM Applying Weight Compression ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 100% • 0:01:08 • 0:00:00
