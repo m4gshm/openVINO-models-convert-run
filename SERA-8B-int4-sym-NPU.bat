059165f083ce52b194536ac81ceb2a003c37c63f
Alexander Bulgakov <mfourgeneralsherman@gmail.com>
2026-08-12 22:21:13 +0300

    fix(gen): gemma4
    # Conflicts:
    #	Qwen2.5-Coder-1.5B-int4-sym-NPU-8887-dev.bat

---
+++ SERA-8B-int4-sym-NPU.bat
@@ -0,0 +1,5 @@
+call agent.bat --model SERA-8B-int4-sym-g128-se-awq ^
+ --device NPU ^
+ --max_prompt_len 49152 ^
+ --attention_backend PA
+
--- gemma-4-E2B-int4-sym-NPU.bat
+++ gemma-4-E2B-int4-sym-NPU.bat
@@ -1,5 +1,5 @@
 call agent.bat --model gemma-4-E2B-it-int4-sym-g128-se-awq ^
- --device NPU --max_prompt_len 49152 ^
+ --device NPU --npu_compiler_type PLUGIN --max_prompt_len 49152 ^
  --attention_backend PA ^
  --generate_config_file .config/generate_config_gemma4_npu.json ^
  --chat_template_file .config/gemma4_chat_template.jinja
