cdd7547d7f8d8739604b01a42290d6ff0f0440f4
mfour <mfourgeneralsherman@gmail.com>
2026-08-15 01:48:42 +0300

    scripts

---
+++ FARA-7B-int4-sym-GPU-dev.bat
@@ -0,0 +1,3 @@
+call agent-dev.bat --model Fara-7B-int4-sym-g128-se-awq ^
+ --device GPU ^
+ --pipe VLM
---
+++ gemma-4-26B-A4B-qat-int4-GPU-dev.bat
@@ -0,0 +1,5 @@
+call agent-dev.bat --model gemma-4-26B-A4B-it-qat-q4_0-unquantized-int4-asym-g64 ^
+ --device GPU ^
+ --pipe VLM ^
+ --generate_config_file .config/generate_config_gemma4.json ^
+ --chat_template_file .config/gemma4_chat_template.jinja
