30bfb4996b4d8e13bea573141749b5285b7e4a17
mfour <mfourgeneralsherman@gmail.com>
2026-08-19 16:48:19 +0300

    fix: npu start

--- gemma-4-E4B-qat-int4-sym-g128-se-awq-GPU-dev.bat
+++ gemma-4-E4B-qat-int4-sym-g128-se-awq-GPU-dev.bat
@@ -1,4 +1,4 @@
-call agent.bat --model gemma-4-E4B-it-qat-q4_0-unquantized-int4-sym-g128-se-awq ^
+call agent-dev.bat --model gemma-4-E4B-it-qat-q4_0-unquantized-int4-sym-g128-se-awq ^
  --device GPU ^
  --pipe VLM ^
  --generate_config_file .config/generate_config_gemma4.json ^
