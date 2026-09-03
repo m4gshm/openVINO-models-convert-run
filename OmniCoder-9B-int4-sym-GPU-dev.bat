02ea8af5d9c1d842e364d40e90d2dfa1b6cb1d8b
Alexander Bulgakov <mfourgeneralsherman@gmail.com>
2026-09-04 01:22:41 +0300

    run: CB optimization

--- OmniCoder-9B-int4-sym-GPU-dev.bat
+++ OmniCoder-9B-int4-sym-GPU-dev.bat
@@ -1,5 +1,4 @@
 call agent-dev.bat --model OmniCoder-9B-int4-sym-g128-se-awq ^
  --detect_cycled_tool_call off ^
  --kv_cache_precision u4 ^
- --pipe CB ^
- --attention_backend PA
+ --pipe CB
--- OmniCoder-9B-int8-sym-GPU-dev.bat
+++ OmniCoder-9B-int8-sym-GPU-dev.bat
@@ -1,4 +1,4 @@
 call agent-dev.bat --model OmniCoder-9B-int8-sym ^
  --detect_cycled_tool_call off ^
  --kv_cache_precision u4 ^
- --pipe CB ^
+ --pipe CB
