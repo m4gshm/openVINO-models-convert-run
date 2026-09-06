5dc10b3db5c3094b1a5736370beda19527c6ed1d
mfour <mfourgeneralsherman@gmail.com>
2026-09-06 18:27:04 +0300

    fix

--- Qwen3.6-35B-A3B-int4-sym-g128-awq-GPU-dev.bat
+++ Qwen3.6-35B-A3B-int4-sym-g128-awq-GPU-dev.bat
@@ -1,4 +1,5 @@
 call agent-dev.bat --model Qwen3.6-35B-A3B-int4-sym-g128-awq ^
  --detect_cycled_tool_call off ^
  --kv_cache_precision u4 ^
+ --generate_config_file qwen_3.6_generate_config.json ^
  --pipe CB
--- Qwen3.6-35B-A3B-int4-sym-g128-awq-GPU.bat
+++ Qwen3.6-35B-A3B-int4-sym-g128-awq-GPU.bat
@@ -1,4 +1,5 @@
 call agent.bat --model Qwen3.6-35B-A3B-int4-sym-g128-awq ^
  --detect_cycled_tool_call off ^
  --kv_cache_precision u4 ^
+ --generate_config_file .config/qwen_3.6_generate_config.json ^
  --pipe CB
